6d8a99d5d78c6e9b587abcdd7ddc06f2573dd171
Alexandre  Ghesquière <Ghesquierea3011@gmail.com>
2020-06-09 11:54:25 +0200

    add debugging

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules, Router } from '@angular/router';
 import { PageLoginComponent } from './login/pages/page-login/page-login.component';
 import { OrdersModule } from './orders/orders.module';
 
@@ -21,7 +21,15 @@ const routes: Routes = [
     ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    enableTracing: false,
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  constructor(router: Router) {
+    const replacer = (key, value) => (typeof value === "function") ? value.name : value;
+    console.log("Routes :", JSON.stringify(router.config, replacer, 2))
+  }
+ }
